8bad68b02714b51b71b2386f3465b1973830559c
caroldmg0905 <99288502+caroldmg0905@users.noreply.github.com>
2022-03-29 16:33:11 -0300

    Add files via upload

---
+++ IncrementoDecremento.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class IncrementoDecremento {
+    
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner(System.in);
+
+       System.out.print("Digite sua idade: ");
+       int idade = entrada.nextInt();
+       System.out.print("Digite um ano futuro: ");
+       int ano = entrada.nextInt();
+
+       int aux = ano - 2022;
+       int idadeFutura = idade + aux;
+
+       System.out.println("Minha idade em " + ano + " é " + idadeFutura);
+
+
+        //int idade = 10;
+        //int novaIdade = idade++;// idade = idade + 1;
+
+        //int novaIdade = idade--;
+        //int novaIdade = --idade;
+
+        //System.out.println("Idade: " + idade);
+        //System.out.println("Nova Idade: " + novaIdade);
+    }
+}
